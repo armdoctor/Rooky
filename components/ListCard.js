8d26d3366081519d97616102f6c70cb2ed9611b2
armdoctor <96279930+armdoctor@users.noreply.github.com>
2023-07-03 20:42:10 +0800

    Update ListCard.js
    Updated the look of the card.

--- components/ListCard.js
+++ components/ListCard.js
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, TouchableOpacity, View, Image } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View, Image, Dimensions } from 'react-native';
 import React, { useEffect, useState } from 'react';
 import { Card, Icon } from 'react-native-elements';
 import { collection, getDocs, query, where } from 'firebase/firestore';
@@ -34,6 +34,8 @@ const fetchListingImage = async (imageName) => {
     return null;
   }
 };
+
+const screenWidth = Dimensions.get('window').width;
 
 const ListCard = ({
   navigation,
@@ -95,9 +97,8 @@ const ListCard = ({
     } catch (error) {
       console.error('Error fetching listing image:', error);
     }
-  };  
-  
-  
+  };
+
   const getListingImageUrl = async (imageName) => {
     try {
       const storageRef = ref(storage, `listingImages/${imageName}`);
@@ -107,8 +108,7 @@ const ListCard = ({
       console.error('Error fetching listing image:', error);
       return ''; // Return a default image URL or handle the error as desired
     }
-  };  
-  
+  };
 
   return (
     <View style={styles.container}>
@@ -125,52 +125,65 @@ const ListCard = ({
           })
         }
       >
-       <Card containerStyle={{ borderRadius: 10, height: 107 }}>
-  {listingImageUrl ? (
-    <Image
-      source={{ uri: listingImageUrl }}
-      style={styles.profileImage}
-    />
-  ) : null}
-  <Text style={styles.headerStyle}>{name}</Text>
-  <CustomRating averageRating={averageRating} />
-  <Text style={styles.subheaderStyle}>${price}/hr</Text>
-</Card>
-
+        <Card containerStyle={styles.cardContainer}>
+          <View style={styles.imageContainer}>
+            {listingImageUrl ? (
+              <Image source={{ uri: listingImageUrl }} style={styles.profileImage} />
+            ) : null}
+          </View>
+          <View style={styles.detailsContainer}>
+            <Text style={styles.headerStyle}>{name}</Text>
+            <Text style={styles.subheaderStyle}>${price}/hr</Text>
+            <CustomRating averageRating={averageRating} />
+          </View>
+        </Card>
       </TouchableOpacity>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  container: {},
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  cardContainer: {
+    width: '90%', // Adjust the width to your desired value
+    borderRadius: 10,
+    backgroundColor: '#fff', // Adjust the background color to match the color scheme
+  },
+  imageContainer: {
+    marginRight: 10,
+    alignItems: 'center',
+  },
+  detailsContainer: {
+    flex: 1,
+  },
   headerStyle: {
-    marginLeft: 100,
-    marginTop: -75,
-    fontSize: 20,
+    fontSize: 24,
     fontWeight: 'bold',
+    marginBottom: 7,
+    marginTop: 10,
+    color: '#333', // Adjust the text color to match the color scheme
   },
   subheaderStyle: {
-    marginLeft: 100,
-    marginTop: 5,
-    fontSize: 16,
-    fontWeight: 'normal',
+    fontSize: 18,
+    marginBottom: 3,
+    color: '#888', // Adjust the text color to match the color scheme
   },
   ratingContainer: {
     flexDirection: 'row',
     marginTop: 6,
-    marginLeft: 100,
     marginBottom: 5,
   },
   starContainer: {
     marginRight: 5,
   },
   profileImage: {
-    width: 75,
-    height: 75,
-    borderRadius: 100,
-    marginTop: 0,
-    marginLeft: 8,
+    width: 300,
+    height: 230,
+    borderRadius: 25,
   },
 });
 
